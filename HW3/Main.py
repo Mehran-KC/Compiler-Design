1dc8d290c011498e3dfd7a1b718eb03cd8a2770f
Mehran Kasipour <KasipourMehran1@gmail.com>
2023-12-15 00:48:54 +0330

    Make URL grammar better and add some colors to terminal with rich library

--- HW3/Main.py
+++ HW3/Main.py
@@ -1,27 +1,106 @@
 # EmailParserMain.py
 
+import re
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+from rich.tree import Tree
 from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
 from gen.TextRecognizerLexer import TextRecognizerLexer  # Update this import
 from gen.TextRecognizerParser import TextRecognizerParser
 from gen.TextRecognizerListener import TextRecognizerListener 
 
-def main():
+
+def national_code_check(code, counter, table, tree) :
+    # CONSOLE WITH TIME AND SMT
+
+    code = str(code)
+    if not code.isnumeric():
+        return False
+    if len(code) != 10:
+        code = '0' * (10 - len(code)) + code
+    total = 0
+    control_digit = int(code[-1])
+    for digit, index in zip(code, range(10,1,-1)):
+        total += int(digit) * index
+
+    reminder = total % 11
+    if reminder < 2:
+        if reminder == control_digit:
+            table.add_row("","","",f"[bold purple]{code}[/bold purple]")
+            tree.children[3].add(f"[bold purple]{code}[/bold purple]")
+            print(f"[{counter}] [bold purple]National Code Found  ==> [/bold purple] {code}")
+            return True
+    else:
+        if 11 - reminder == control_digit:
+            table.add_row("","","",f"[bold purple]{code}[/bold purple]")
+            tree.children[3].add(f"[bold purple]{code}[/bold purple]")
+            print(f"[{counter}] [bold purple]National Code Found  ==> [/bold purple] {code}")
+            return True
+    return False
+
+def validate_postal_code(postal_code):
+    pattern = r'\b(?!(\d)\1{3})[13-9]{4}[1346-9][013-9]{5}\b'
+    return bool(re.fullmatch(pattern, postal_code))
+
+    
+def main(table, tree):
+    counter = 1
     
     input_file = "input.txt" 
     input_stream = FileStream(input_file)
     lexer = TextRecognizerLexer(input_stream)
+    
     for token in lexer.getAllTokens():
-        if(token.type == 3):
-            print("Found email:", token.text)
-        if(token.type == 4):
-            print("MELLI CODE Found:", token.text)
-        if(token.type == 5):
-            print("Valid URL ---->", token.text)
+        if (token.type == 6):
+            if national_code_check(token.text,counter, table, tree):
+                counter+=1
+                continue
+            national_code_check(token.text,counter, table, tree)
+
+            if validate_postal_code(token.text):
+                table.add_row("","",f"[bold yellow]{token.text}[/bold yellow]")
+                tree.children[2].add(f"[bold yellow]{token.text}[/bold yellow]")
+                print(f"[{counter}][bold yellow] Postal Code Found ==> [/bold yellow] [bold Green]{token.text}[/bold Green]")
+                counter+=1
+
+        elif (token.type == 7 ):
+            table.add_row(f"[bold Magenta]{token.text}[/bold Magenta]")
+            tree.children[0].add(f"[bold Magenta]{token.text}[/bold Magenta]")
+            print(f"[{counter}][bold Magenta] Email FOUND ==> [/bold Magenta] [bold Green]{token.text}[/bold Green]")
+            counter+=1
+
+        elif (token.type == 8):
+            table.add_row("",f"[bold red]{token.text}[/bold red]")
+            tree.children[1].add(f"[bold red]{token.text}[/bold red]")
+            print(f"[{counter}][bold red] URL FOUND  ==> [/bold red] [bold Green]{token.text}[/bold Green]")
+            counter+=1
+
+
     token_stream = CommonTokenStream(lexer)
     parser = TextRecognizerParser(token_stream)
     tree = parser.emails()
     walker = ParseTreeWalker()
 
 if __name__ == "__main__":
-    main()
 
+    tree = Tree("")
+    tree.add("Emails")
+    tree.add("URLs")
+    tree.add("Postal Codes")
+    tree.add("National Codes")
+    tree.centering = "center"
+    table = Table(title="[bold]Text Recognizer[/bold]")
+    table.add_column("Emails", justify="center", style="cyan", no_wrap=True)
+    table.add_column("URLs", justify="center", style="magenta", no_wrap=True)
+    table.add_column("Postal Codes", justify="center", style="green", no_wrap=True)
+    table.add_column("National Codes", justify="center", style="red", no_wrap=True)
+
+    main(table, tree)
+    print("\n")
+    console = Console()
+    console.print(Panel(table, expand=False, border_style="green"))
+    print("\n")
+    #print tree as panel
+    console.print(tree, style="bold")
--- HW3/gen/TextRecognizerParser.py
+++ HW3/gen/TextRecognizerParser.py
@@ -10,17 +10,17 @@ else:
 
 def serializedATN():
     return [
-        4,1,8,35,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,1,0,1,0,1,0,1,0,3,0,13,
+        4,1,26,33,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,1,0,1,0,1,0,1,0,3,0,13,
         8,0,1,0,1,0,1,0,1,0,1,0,3,0,20,8,0,5,0,22,8,0,10,0,12,0,25,9,0,1,
-        1,1,1,1,1,1,2,1,2,1,3,1,3,1,3,1,3,0,0,4,0,2,4,6,0,0,37,0,12,1,0,
-        0,0,2,26,1,0,0,0,4,29,1,0,0,0,6,31,1,0,0,0,8,13,3,2,1,0,9,13,3,4,
-        2,0,10,13,3,6,3,0,11,13,5,0,0,1,12,8,1,0,0,0,12,9,1,0,0,0,12,10,
-        1,0,0,0,12,11,1,0,0,0,13,23,1,0,0,0,14,19,5,2,0,0,15,20,3,2,1,0,
-        16,20,3,4,2,0,17,20,3,6,3,0,18,20,5,0,0,1,19,15,1,0,0,0,19,16,1,
-        0,0,0,19,17,1,0,0,0,19,18,1,0,0,0,20,22,1,0,0,0,21,14,1,0,0,0,22,
-        25,1,0,0,0,23,21,1,0,0,0,23,24,1,0,0,0,24,1,1,0,0,0,25,23,1,0,0,
-        0,26,27,5,3,0,0,27,28,6,1,-1,0,28,3,1,0,0,0,29,30,5,4,0,0,30,5,1,
-        0,0,0,31,32,5,5,0,0,32,33,6,3,-1,0,33,7,1,0,0,0,3,12,19,23
+        1,1,1,1,2,1,2,1,3,1,3,1,3,0,0,4,0,2,4,6,0,0,35,0,12,1,0,0,0,2,26,
+        1,0,0,0,4,28,1,0,0,0,6,30,1,0,0,0,8,13,3,2,1,0,9,13,3,4,2,0,10,13,
+        3,6,3,0,11,13,5,0,0,1,12,8,1,0,0,0,12,9,1,0,0,0,12,10,1,0,0,0,12,
+        11,1,0,0,0,13,23,1,0,0,0,14,19,5,5,0,0,15,20,3,2,1,0,16,20,3,4,2,
+        0,17,20,3,6,3,0,18,20,5,0,0,1,19,15,1,0,0,0,19,16,1,0,0,0,19,17,
+        1,0,0,0,19,18,1,0,0,0,20,22,1,0,0,0,21,14,1,0,0,0,22,25,1,0,0,0,
+        23,21,1,0,0,0,23,24,1,0,0,0,24,1,1,0,0,0,25,23,1,0,0,0,26,27,5,7,
+        0,0,27,3,1,0,0,0,28,29,5,6,0,0,29,5,1,0,0,0,30,31,5,8,0,0,31,7,1,
+        0,0,0,3,12,19,23
     ]
 
 class TextRecognizerParser ( Parser ):
@@ -33,10 +33,16 @@ class TextRecognizerParser ( Parser ):
 
     sharedContextCache = PredictionContextCache()
 
-    literalNames = [ "<INVALID>", "':'", "','" ]
+    literalNames = [ "<INVALID>", "'http'", "'https'", "'/'", "':'", "','" ]
 
-    symbolicNames = [ "<INVALID>", "COLON", "COMMA", "EMAIL", "MELLI_NUMBER", 
-                      "URL", "PROTOCOL", "DIGIT", "WS" ]
+    symbolicNames = [ "<INVALID>", "HTTP", "HTTPS", "SLASH", "COLON", "COMMA", 
+                      "MELLI_NUMBER", "EMAIL", "URL", "URL_HOSTNAME", "URL_SCHEME", 
+                      "URL_HOSTNAME_IPV4", "URL_HOSTNAME_IPV6", "URL_HOSTNAME_IPV6_HEX", 
+                      "URL_HOSTNAME_IPV6_HEX_PART", "URL_HOSTNAME_IPV6_HEX_DIGIT", 
+                      "URL_HOSTNAME_DOMAIN", "URL_HOSTNAME_DOMAIN_PART", 
+                      "URL_PORT", "URL_PATH", "URL_PATH_PART", "URL_QUERY", 
+                      "URL_QUERY_PART", "URL_FRAGMENT", "URL_FRAGMENT_PART", 
+                      "DIGIT", "WS" ]
 
     RULE_emails = 0
     RULE_email = 1
@@ -46,14 +52,32 @@ class TextRecognizerParser ( Parser ):
     ruleNames =  [ "emails", "email", "melli_number", "url" ]
 
     EOF = Token.EOF
-    COLON=1
-    COMMA=2
-    EMAIL=3
-    MELLI_NUMBER=4
-    URL=5
-    PROTOCOL=6
-    DIGIT=7
-    WS=8
+    HTTP=1
+    HTTPS=2
+    SLASH=3
+    COLON=4
+    COMMA=5
+    MELLI_NUMBER=6
+    EMAIL=7
+    URL=8
+    URL_HOSTNAME=9
+    URL_SCHEME=10
+    URL_HOSTNAME_IPV4=11
+    URL_HOSTNAME_IPV6=12
+    URL_HOSTNAME_IPV6_HEX=13
+    URL_HOSTNAME_IPV6_HEX_PART=14
+    URL_HOSTNAME_IPV6_HEX_DIGIT=15
+    URL_HOSTNAME_DOMAIN=16
+    URL_HOSTNAME_DOMAIN_PART=17
+    URL_PORT=18
+    URL_PATH=19
+    URL_PATH_PART=20
+    URL_QUERY=21
+    URL_QUERY_PART=22
+    URL_FRAGMENT=23
+    URL_FRAGMENT_PART=24
+    DIGIT=25
+    WS=26
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
@@ -220,7 +244,6 @@ class TextRecognizerParser ( Parser ):
             self.enterOuterAlt(localctx, 1)
             self.state = 26
             self.match(TextRecognizerParser.EMAIL)
-            print("**email found**")
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
@@ -260,7 +283,7 @@ class TextRecognizerParser ( Parser ):
         self.enterRule(localctx, 4, self.RULE_melli_number)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 29
+            self.state = 28
             self.match(TextRecognizerParser.MELLI_NUMBER)
         except RecognitionException as re:
             localctx.exception = re
@@ -301,9 +324,8 @@ class TextRecognizerParser ( Parser ):
         self.enterRule(localctx, 6, self.RULE_url)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 31
+            self.state = 30
             self.match(TextRecognizerParser.URL)
-            print("**URL found**")
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
